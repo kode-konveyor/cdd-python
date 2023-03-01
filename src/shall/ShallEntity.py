60e55563ebb8442093d93f4fefd47f8ea11a61bc
Arpad Magosanyi <m4gw4s@gmail.com>
2023-03-01 12:02:49 +0100

    more https://github.com/python/mypy/issues/14622 bug

--- src/shall/ShallEntity.py
+++ src/shall/ShallEntity.py
@@ -10,6 +10,6 @@ class ShallEntity(Generic[P, R]):
     callable: Callable[P, R]
     returnConstraints: list[tuple[str, Callable[Concatenate[R, P], bool]]]
     sideEffectCheckers: list[tuple[str, SideEffectChecker[P, R]]]
-    parameters: tuple[P.args, P.kwargs]
+    parameters: tuple[P.args, P.kwargs] #type:ignore #https://github.com/python/mypy/issues/14622
     returnValue: R
     explanation: str
